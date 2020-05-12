b7337557ef6523da061305ca9932840b48157865
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-05-12 11:13:33 +0100

    bridges | fix: decoding tokens for forward strands

--- bridges.py
+++ bridges.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('src')
+
+sys.path.append("src")
 import numpy as np
 from bridger import Model
 
@@ -14,14 +15,16 @@ prefix = "Aha ! À nous les ponts !"
 suffix = "Et après ces travaux ils virent que les ponts étaient bons."[::-1]
 
 fw_tokens, _, scores, _ = fw_model.run(prefix=prefix, length=20)
-# the backwards strands are generated backwards 
+# the backwards strands are generated backwards
 bw_strands_rev = bw_model.gen(prefix=suffix, length=20)
 
 # cuts the strand so that we don't end it in the middle of a word
-def cleanup_strand(strand): 
-    return strand[:strand.rfind(' ')]
+def cleanup_strand(strand):
+    return strand[: strand.rfind(" ")]
+
 
 # cut at the last space
+fw_strands = fw_model.decode(fw_tokens)
 fw_strands = [cleanup_strand(fw_model.decode(fw_tokens)) for strand in fw_strands]
 bw_strands = [cleanup_strand(strand_rev)[::-1] for strand_rev in bw_strands_rev]
 
@@ -29,8 +32,14 @@ bw_strands = [cleanup_strand(strand_rev)[::-1] for strand_rev in bw_strands_rev]
 # bw_strands = [" Ils souffrirent deux ou trois lustres.", " Je les avais prévenus, mais ils n'en firent qu'à leur tête."]
 
 # these are the locations where we may want to cut the strands for recombinations
-all_fw_cut_indices = [[i for i, letter in enumerate(fw_strand) if letter == ' '] for fw_strand in fw_strands]
-all_bw_cut_indices = [[i for i, letter in enumerate(bw_strand) if letter == ' '] for bw_strand in bw_strands]
+all_fw_cut_indices = [
+    [i for i, letter in enumerate(fw_strand) if letter == " "]
+    for fw_strand in fw_strands
+]
+all_bw_cut_indices = [
+    [i for i, letter in enumerate(bw_strand) if letter == " "]
+    for bw_strand in bw_strands
+]
 
 # returns a (fairly long) list of possible bridges, that we will then evaluate
 # through their forward likelihood
@@ -45,6 +54,7 @@ def generate_possible_bridges(fw_strands, bw_strands):
                     possible_bridges.append(possible_bridge)
     return possible_bridges
 
+
 perps = fw_model.get_perplexity(possible_bridges)
 sorted_indz = perps[:, 0].argsort()[::-1]
 sorted_perps = perps[sorted_indz]
@@ -53,4 +63,4 @@ for sentence, perp in zip(possible_bridges, perps):
     print()
     print(sentence)
     print(f"\t\tperp -----> {perp[0]:.16f}")
-    print('-'*40)
+    print("-" * 40)
