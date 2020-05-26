909e281005bb1e1156e736bbb75f828b10a14fcd
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-05-26 12:14:36 +0100

    zephyrus sh | two scripts for local testing

---
+++ zephyrus-concat.sh
@@ -0,0 +1,6 @@
+PYTHONPATH=src python bridges.py \
+  --mode tokens \
+  --concat \
+  --run_name_fw run1 \
+  --model_name_fw 117M \
+  --run_name_bw 117Mr
---
+++ zephyrus-overlap.sh
@@ -0,0 +1,8 @@
+PYTHONPATH=src python bridges.py \
+  --mode tokens \
+  --overlap \
+  --run_name_fw run1 \
+  --model_name_fw 117M \
+  --run_name_bw 117Mr \
+  --ngrams 4
+
