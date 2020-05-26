d27257f5d139fab82291c2b873467f7d33759ec7
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-05-26 12:12:36 +0100

    bridges sh | updating Leviathan script

--- bridges.sh
+++ bridges.sh
@@ -1,2 +1,9 @@
 conda activate tf14
-CUDA_VISIBLE_DEVICES=0 PYTHONPATH=src python bridges.py
+CUDA_VISIBLE_DEVICES=0 PYTHONPATH=src python bridges.py \
+  --mode letters \
+  --overlap \
+  --batch_size 10 \
+  --length 200 \
+  --ngrams 5 \
+  --write
+
