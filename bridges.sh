7eac8fd71a2fefc6f53527e3c0cf7a8864541e76
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-05-26 14:55:14 +0100

    bridges sh | adding model names

--- bridges.sh
+++ bridges.sh
@@ -2,6 +2,8 @@ conda activate tf14
 CUDA_VISIBLE_DEVICES=0 PYTHONPATH=src python bridges.py \
   --mode letters \
   --overlap \
+  --model_name_fw 1558M \
+  --model_name_bw 1558M \
   --batch_size 10 \
   --length 200 \
   --ngrams 5 \
