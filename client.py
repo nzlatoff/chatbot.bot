7b1dc9f2f43fd285e509f2f4506bd70a7bee2f57
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-08-17 09:55:48 +0100

    client | blackened, send_config

--- client.py
+++ client.py
@@ -35,17 +35,11 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--temperature",
-    type=float,
-    default=0.9,
-    help="Temperature when sampling.",
+    "--temperature", type=float, default=0.9, help="Temperature when sampling.",
 )
 
 parser.add_argument(
-    "--top_p",
-    type=float,
-    default=0.998,
-    help="Nucleus sampling when sampling.",
+    "--top_p", type=float, default=0.998, help="Nucleus sampling when sampling.",
 )
 
 parser.add_argument(
@@ -56,9 +50,7 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--local",
-    action="store_true",
-    help="Run with local server, port 5100.",
+    "--local", action="store_true", help="Run with local server, port 5100.",
 )
 
 parser.add_argument(
@@ -86,7 +78,7 @@ args = parser.parse_args()
 sio = socketio.Client(logger=False, reconnection_delay_max=50)
 
 le_model = Model(model_name=args.model, run_name=args.run_name)
-print("-"*40)
+print("-" * 40)
 print("(settings:)")
 print()
 for k, v in vars(args).items():
@@ -104,6 +96,7 @@ resetting_session = False
 messages = []
 prefix = ""
 
+
 def generate(rank_threshold=25):
 
     global is_generating
@@ -114,7 +107,8 @@ def generate(rank_threshold=25):
 
     is_generating = True
 
-    if should_sess_be_reset(): return
+    if should_sess_be_reset():
+        return
 
     # print("-"*40)
     # print("prefix:")
@@ -130,15 +124,17 @@ def generate(rank_threshold=25):
     # add end of answer, store length of prefix
     end_pref = len(prefix)
 
-    l = le_model.gen(prefix=prefix,
-                     temperature=args.temperature,
-                     top_p=args.top_p,
-                     length=args.length_desired)[0]
-    l = regex.sub(r"<\|endoftext\|>", "", l) # no openai marker
+    l = le_model.gen(
+        prefix=prefix,
+        temperature=args.temperature,
+        top_p=args.top_p,
+        length=args.length_desired,
+    )[0]
+    l = regex.sub(r"<\|endoftext\|>", "", l)  # no openai marker
     generated = l[end_pref:]
 
     print()
-    print("\t\t\t" + "-"*40)
+    print("\t\t\t" + "-" * 40)
     print("\t\t\t(raw)")
     print()
     msg = "\n\t\t\t".join(textwrap.wrap(generated, width=40))
@@ -153,17 +149,17 @@ def generate(rank_threshold=25):
             char = ""
             message = regex.sub("<\|[es]\|>", "", repl)
         else:
-            char = regex.sub("<\|[es]\|>", "", repl[:repl.find("\n")])
-            message = regex.sub("<\|[es]\|>", "", repl[repl.find("\n") + 1:])
+            char = regex.sub("<\|[es]\|>", "", repl[: repl.find("\n")])
+            message = regex.sub("<\|[es]\|>", "", repl[repl.find("\n") + 1 :])
 
-        print("\t\t" + "-"*40)
+        print("\t\t" + "-" * 40)
         print("\t\t(generated)")
         print()
         rmsg = "\n\t\t".join(textwrap.wrap(repl, width=40))
         print(f"\t\t{rmsg}")
         print()
 
-        print("\t" + "-"*40)
+        print("\t" + "-" * 40)
         print("\t(char)")
         print()
         print(f"\t{char}")
@@ -189,7 +185,7 @@ def generate(rank_threshold=25):
         print()
 
         if le_rank < rank_threshold:
-            print("-"*40)
+            print("-" * 40)
             print(char)
             msg = "\n".join(textwrap.wrap(message, width=40))
             print(msg)
@@ -198,15 +194,26 @@ def generate(rank_threshold=25):
             if args.network_print_speed > 0:
                 for i in range(len(message) + 1):
 
-                    if should_sess_be_reset(): return
+                    if should_sess_be_reset():
+                        return
 
                     # print({ "id": sio.sid, "character": char, "message": # message[:i], "user": args.server_name})
-                    send_typing({ "id": sio.sid, "character": char, "message": message[:i], "user": args.server_name})
+                    send_typing(
+                        {
+                            "id": sio.sid,
+                            "character": char,
+                            "message": message[:i],
+                            "user": args.server_name,
+                        }
+                    )
                     time.sleep(args.network_print_speed)
 
-            if should_sess_be_reset(): return
+            if should_sess_be_reset():
+                return
 
-            send_message({ "character": char, "message": message, "user": args.server_name})
+            send_message(
+                {"character": char, "message": message, "user": args.server_name}
+            )
             prefix = f"{prefix}{start}{char}\n{message}"
         else:
             print()
@@ -214,7 +221,7 @@ def generate(rank_threshold=25):
             print()
     else:
         msg = "\n\t\t".join(textwrap.wrap(l, width=40))
-        print("\t\t" + "-"*40)
+        print("\t\t" + "-" * 40)
         print("\t\t(generated:)")
         print("\t\t" + msg)
         print()
@@ -222,7 +229,9 @@ def generate(rank_threshold=25):
         print()
 
     is_generating = False
-    if should_sess_be_reset(): return
+    if should_sess_be_reset():
+        return
+
 
 def should_sess_be_reset():
     global resetting_session
@@ -230,27 +239,30 @@ def should_sess_be_reset():
     if resetting_session:
         print(f"generation interrupted")
         print()
-        print("="*40)
+        print("=" * 40)
         is_generating = False
         resetting_session = False
         return True
 
+
 @sio.event
 def connect():
     print("connection established")
-    print("-"*40)
-    print()
+    print("-" * 40)
     sio.emit("new bot", args.server_name)
+
 
 @sio.event
 def connect_error(e):
     print("The connection failed!")
     print(e)
 
+
 @sio.event
 def disconnect():
     print("connection lost")
-    print("-"*40)
+    print("-" * 40)
+
 
 @sio.on("erase messages")
 def reset_session():
@@ -260,7 +272,7 @@ def reset_session():
     global prefix
 
     print()
-    print("="*40)
+    print("=" * 40)
     print()
     print("resetting session")
     messages = []
@@ -269,7 +281,8 @@ def reset_session():
         resetting_session = True
     else:
         print()
-        print("="*40)
+        print("=" * 40)
+
 
 @sio.on("received")
 def on_chat_message(data):
@@ -280,11 +293,13 @@ def on_chat_message(data):
     char = data["character"].replace("\n", "\t\n")
     msg = data["message"].replace("\n", "\t\n")
 
-    print("\t" + "-"*40)
+    print("\t" + "-" * 40)
     print("\t(received)")
     print()
-    if data["character"]: print(f"\t{char}")
-    if data["message"]: print(f"\t{msg}")
+    if data["character"]:
+        print(f"\t{char}")
+    if data["message"]:
+        print(f"\t{msg}")
 
     messages.append(data)
     character = data["character"]
@@ -299,7 +314,7 @@ def on_chat_message(data):
     # print("-"*40)
 
     rand = random.random()
-    print("\t" + "-"*40)
+    print("\t" + "-" * 40)
     print(f"\trandom has spoken: {rand}")
     if not is_generating:
         if rand > 0:
@@ -311,8 +326,27 @@ def on_chat_message(data):
         print()
 
 
+@sio.on("get bot config")
+def send_config():
+    sio.emit(
+        "config from bot",
+        {
+            "id": sio.sid,
+            "user": args.server_name,
+            "model": args.model,
+            "run": args.run_name,
+            "temperature": args.temperature,
+            "top p": args.top_p,
+            "print speed": args.network_print_speed,
+            "length desired": args.length_desired,
+            "rank threshold": args.rank_threshold,
+        },
+    )
+
+
 def send_typing(data):
     sio.emit("typing", data)
+
 
 def send_message(data):
     # print("-"*40)
@@ -321,22 +355,23 @@ def send_message(data):
     # print("-"*40)
     sio.emit("chat message", data)
 
+
 user_pass = b64encode(b"guest:vuVpm77e").decode("ascii")
 if args.local:
     url = "http://localhost:5100"
-    print("-"*40)
+    print("-" * 40)
     print(f"connecting to: {url}")
     sio.connect(url)
 else:
     if args.heroku:
         url = "https://shrouded-stream-73690.herokuapp.com/"
-        print("-"*40)
+        print("-" * 40)
         print(f"connecting to: {url}")
         sio.connect(url)
     else:
         user_pass = b64encode(b"guest:vuVpm77e").decode("ascii")
         url = "https://spark.theatrophone.fr"
-        print("-"*40)
+        print("-" * 40)
         print(f"connecting to: {url}")
-        sio.connect(url,  { "Authorization" : "Basic %s" % user_pass})
+        sio.connect(url, {"Authorization": "Basic %s" % user_pass})
 sio.wait()
